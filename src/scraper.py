35cb81e88f2c65d9b9fa0c606ced07e790483033
Habibina Arif Muzayyan <fira.anibibah@gmail.com>
2025-03-17 19:15:11 +0700

    bukhari.json

--- src/scraper.py
+++ src/scraper.py
@@ -4,15 +4,14 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
-        "https://www.hadits.id/hadits/bukhari/"+str(i) for i in range(1,7009)
+        "https://hadits.in/bukhari/"+str(i) for i in range(1,7009)
     ]
 
     def parse(self, response):
-        title = response.css("div.hadits-about")
-        content = response.css("article.hadits-content")
+        title = response.css("title::text").get()
         yield {
-            "Nomor": title.css("h2::text").get()[29:].split(" - ",1)[0],
-            "Bab": title.css("h2::text").get()[29:].split(" - ",1)[1],
-            "Tentang": content.css("h1::text").get(),
-            "Terjemahan": content.css("a.copy-btn").attrib["data-string"].split(" https://")[0],
+            "Nomor": title[15:].split(" tentang '")[0],
+            "Kitab": title[15:].split(" tentang '")[1].split(": ",1)[0],
+            "Bab": title[15:].split(" tentang '")[1].split(": ",1)[1],
+            "indo": response.xpath("//meta[@name='description']/@content").extract(),
         }
